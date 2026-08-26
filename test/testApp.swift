27b2e5886dc48bbb735b60a21bfaf6e792d76241
Henrique Carhuapoma Capillo <hcarhuapomacc@gmail.com>
2026-08-25 20:59:42 -0500

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Henrique Carhuapoma Capillo on 25/08/26.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
